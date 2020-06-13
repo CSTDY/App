9048c94eba2052989397f83059cd7ac646843863
CSTDY <kamillsocha2000@gmail.com>
2020-06-13 19:26:23 +0200

    Commit Zadanie_1 i 2

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Car {
+    final String model;
+    final String producer;
+    Double weight;
+    String plate;
+    File pic;
+
+    public Car(String model, String producer) {
+        this.model = model;
+        this.producer=producer;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Human me = new Human();
+        me.firstName = "Kamil";
+        me.lastName = "Socha";
+
+        Animal dog = new Animal("dog");
+        me.pet= dog;
+        me.pet.feed();
+        for(int i=0;i<14;i++){
+            me.pet.takeForAWalk();}
+        Car seat = new Car("Ibiza", "seat");
+        me.auto = seat;
     }
+
 }
